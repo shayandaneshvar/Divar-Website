1c3758b6d6b21949644311e94416bc099e97cae9
shayandaneshvar <daneshvarshayan@gmail.com>
2020-07-18 19:49:14 +0430

    Add index.html

--- src/main/java/ir/ac/kntu/divar/controller/MainController.java
+++ src/main/java/ir/ac/kntu/divar/controller/MainController.java
@@ -1,8 +1,21 @@
 package ir.ac.kntu.divar.controller;
 
+import ir.ac.kntu.divar.model.service.LocationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class MainController {
+    private final LocationService service;
+
+    @GetMapping(value = {"/","index","index.html"})
+    public String getIndex(Model model) {
+        model.addAttribute("cities", service.getAllCities());
+        return "index";
+    }
 
 }
