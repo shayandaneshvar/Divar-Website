b6a5f373037b05f4b53e11398c24f7570d1f30a7
shayandaneshvar <daneshvarshayan@gmail.com>
2020-07-20 15:24:52 +0430

    add All Ads Html page and controller

--- src/main/java/ir/ac/kntu/divar/controller/AdvertisementController.java
+++ src/main/java/ir/ac/kntu/divar/controller/AdvertisementController.java
@@ -1,9 +1,24 @@
 package ir.ac.kntu.divar.controller;
 
+import ir.ac.kntu.divar.model.service.advertisement.AdvertisementService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/ads")
+@RequestMapping("/{city}")
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class AdvertisementController {
+    private final AdvertisementService advertisementService;
+
+    @GetMapping()
+    public String getAds(@PathVariable String city, Model model) {
+        model.addAttribute("ads", advertisementService.getAllByCity(city));
+        model.addAttribute("city", city);
+        return "all-ads";
+    }
 }
--- src/main/java/ir/ac/kntu/divar/model/service/advertisement/AdvertisementService.java
+++ src/main/java/ir/ac/kntu/divar/model/service/advertisement/AdvertisementService.java
@@ -1,10 +1,28 @@
 package ir.ac.kntu.divar.model.service.advertisement;
 
+import ir.ac.kntu.divar.controller.VehicleController;
+import ir.ac.kntu.divar.model.entity.advertisement.Advertisement;
+import ir.ac.kntu.divar.model.service.advertisement.electronics.ElectronicsService;
+import ir.ac.kntu.divar.model.service.advertisement.realestate.RealEstateService;
+import ir.ac.kntu.divar.model.service.advertisement.vehicle.VehicleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class AdvertisementService {
+    private final ElectronicsService electronicsService;
+    private final VehicleService vehicleService;
+    private final RealEstateService realEstateService;
+    public List<? extends Advertisement> getAllByCity(String city) {
+        List<Advertisement> list =
+                new ArrayList<>(electronicsService.getAllByCity(city));
+        list.addAll(vehicleService.getAllByCity(city));
+        list.addAll(realEstateService.getAllByCity(city));
+        return list;
+    }
 }
